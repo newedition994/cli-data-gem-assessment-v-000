3665cbcbac05f5c50ff6a3b914f3b0e87bc9edd3
Breyanna Vass <breyanna.bvass@gmail.com>
2017-09-21 15:32:22 +0000

    Done.

---
+++ shop-tops-cli-app/shop-tops-cli-app/lib/shop/tops/cli/app.rb
@@ -0,0 +1,11 @@
+require "shop/tops/cli/app/version"
+
+module Shop
+  module Tops
+    module Cli
+      module App
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ shop-tops-cli-app/shop-tops-cli-app/lib/shop/tops/cli/app/version.rb
@@ -0,0 +1,9 @@
+module Shop
+  module Tops
+    module Cli
+      module App
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
