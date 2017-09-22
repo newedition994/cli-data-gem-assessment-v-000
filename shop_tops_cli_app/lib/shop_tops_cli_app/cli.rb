70a550f9642e99b1b75021962085ea5b6d13ede9
Breyanna Vass <breyanna.bvass@gmail.com>
2017-09-22 21:28:28 +0000

    Done.

--- shop_tops_cli_app/lib/shop_tops_cli_app/cli.rb
+++ shop_tops_cli_app/lib/shop_tops_cli_app/cli.rb
@@ -2,6 +2,17 @@ class ShopTopsCliApp::CLI
 
   def call
     puts "Check out these cute Tops:"
+    list_types
+  end
+
+  def list_types
+    puts <<~HEREDOC
+      1. Lace-Up
+      2. Tees & Tanks
+      3. Shirts & Blouses
+      4. Sweaters & Cardigans
+      5. Going Out Tops
+    HEREDOC
   end
 
 
