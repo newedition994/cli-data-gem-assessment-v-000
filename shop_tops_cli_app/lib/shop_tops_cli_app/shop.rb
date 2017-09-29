009c109f060445a119b21c23833c07096051ad3c
Breyanna Vass <breyanna.bvass@gmail.com>
2017-09-29 12:41:40 +0000

    Done.

--- shop_tops_cli_app/lib/shop_tops_cli_app/shop.rb
+++ shop_tops_cli_app/lib/shop_tops_cli_app/shop.rb
@@ -2,7 +2,7 @@ class ShopTopsCliApp::Shop
 
   attr_accessor :name, :url
 
-  def initialize(name, url)
+  def initialize
     @name = name
     @url = url
   end
@@ -11,29 +11,29 @@ class ShopTopsCliApp::Shop
   def self.forever
     #should return a bunch of instances of categories
     puts <<~HEREDOC
-      1. Lace-Up
+      "1. Lace-Up
       2. Tees & Tanks
       3. Shirts & Blouses
       4. Sweaters & Cardigans
-      5. Going Out Tops
+      5. Going Out Tops"
     HEREDOC
-    shop_1 = Shop.new
+    shop_1 = self.new
     shop_1.name = "Lace-Up"
     shop_1.url = "https://www.forever21.com/us/shop/Catalog/Category/f21/top_blouses-lace-up"
 
-    shop_2 = Shop.new
+    shop_2 = self.new
     shop_2.name = "Tees & Tanks"
     shop_2.url = "https://www.forever21.com/us/shop/Catalog/Category/f21/top_t-shirts"
 
-    shop_3 = Shop.new
+    shop_3 = self.new
     shop_3.name = "Shits & Blouses"
     shop_3.url = "https://www.forever21.com/us/shop/Catalog/Category/f21/top_blouses-shirts"
 
-    shop_4 = Shop.new
+    shop_4 = self.new
     shop_4.name = "Sweaters & Cardigans"
     shop_4.url = "https://www.forever21.com/us/shop/Catalog/Category/f21/sweater"
 
-    shop_5 = Shop.new
+    shop_5 = self.new
     shop_5.name = "Going Out Tops"
     shop_5.url = "https://www.forever21.com/us/shop/Catalog/Category/f21/top_going-out-tops"
 
