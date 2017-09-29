17cccf34ef35f1bf5a882515cb1d2df216b4973c
Breyanna Vass <breyanna.bvass@gmail.com>
2017-09-29 14:40:40 +0000

    Done.

--- shop_tops_cli_app/lib/shop_tops_cli_app/shop.rb
+++ shop_tops_cli_app/lib/shop_tops_cli_app/shop.rb
@@ -9,30 +9,16 @@ class ShopTopsCliApp::Shop
 
 
   def self.forever
-    #should return a bunch of instances of categories
-    
-    shop_1 = self.new
-    shop_1.name = "Lace-Up"
-    shop_1.url = "https://www.forever21.com/us/shop/Catalog/Category/f21/top_blouses-lace-up"
+    #Scrape forever21 tops sections and then return products and prices based on the data
+    self.scrape_shop
+  end
 
-    shop_2 = self.new
-    shop_2.name = "Tees & Tanks"
-    shop_2.url = "https://www.forever21.com/us/shop/Catalog/Category/f21/top_t-shirts"
-
-    shop_3 = self.new
-    shop_3.name = "Shits & Blouses"
-    shop_3.url = "https://www.forever21.com/us/shop/Catalog/Category/f21/top_blouses-shirts"
-
-    shop_4 = self.new
-    shop_4.name = "Sweaters & Cardigans"
-    shop_4.url = "https://www.forever21.com/us/shop/Catalog/Category/f21/sweater"
-
-    shop_5 = self.new
-    shop_5.name = "Going Out Tops"
-    shop_5.url = "https://www.forever21.com/us/shop/Catalog/Category/f21/top_going-out-tops"
-
-    [shop_1, shop_2, shop_3, shop_4, shop_5]
-
+  def self.scrape_shop
+    shop = []
+    #Go to forever21 tops categeory, find various top sections
+    #extract the sections
+    #instantiate the top sections
+    shop
   end
 
 
