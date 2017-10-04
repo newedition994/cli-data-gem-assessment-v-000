c3db99460316c29c9e1c8e3288028b40f0819ebb
Breyanna Vass <breyanna.bvass@gmail.com>
2017-10-04 19:22:49 +0000

    Done.

--- shop_tops_cli_app/lib/shop_tops_cli_app/shop.rb
+++ shop_tops_cli_app/lib/shop_tops_cli_app/shop.rb
@@ -16,18 +16,38 @@ class ShopTopsCliApp::Shop
   def self.scrape_shop
     shop = []
 
-    shop << self.scrape_ebay
+    shop << self.scrape_shoulder
+    shop << self.scrape_blouses
+    shop << self.scrape_tunic
+    shop << self.scrape_tank
+    shop << self.scrape_button
     #Go to ebay tops categeory, find various top sections
     #extract the sections
     #instantiate the top sections
     shop
   end
 
-  def self.scrape_ebay
-    doc = Nokogiri::HTML(open("https://www.ebay.com/b/Womens-Tops-Blouses/53159/bn_661824"))
-    name = doc.search('p.b-guidancecard__title')[0..4].text.strip
-    url = doc.search('a.b-guidancecard__link').first.attr('href')
-    #binding.pry
+  def self.scrape_shoulder
+    doc = Nokogiri::HTML(open("https://www.ebay.com/b/Womens-Off-Shoulder-Tops-Blouses/53159/bn_651757"))
+    name = doc.search('p.b-guidancecard__title')[0].text.strip
+    #url = doc.search('a.b-guidancecard__link').first.attr('href')
+    binding.pry
+  end
+
+  def self.scrape_blouses
+    doc = Nokogiri::HTML(open("https://www.ebay.com/b/Womens-Blouses/53159/bn_649768"))
+  end
+
+  def self.scrape_tunic
+    doc = Nokogiri::HTML(open("https://www.ebay.com/b/Tunic-Tops/53159/bn_635701"))
+  end
+
+  def self.scrape_tank
+    doc = Nokogiri::HTML(open("https://www.ebay.com/b/Womens-Tank-Cami-Tops-Blouses/53159/bn_652021"))
+  end
+
+  def self.scrape_button
+    doc = Nokogiri::HTML(open("https://www.ebay.com/b/Button-Down-Shirts-for-Women/53159/bn_628831"))
   end
 
 end
