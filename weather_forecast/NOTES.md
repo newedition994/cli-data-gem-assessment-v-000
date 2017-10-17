3451ac2839272322171825221b43cff0cb18ba64
Breyanna Vass <breyanna.bvass@gmail.com>
2017-10-17 17:15:33 +0000

    Done.

---
+++ weather_forecast/NOTES.md
@@ -0,0 +1,6 @@
+Forecast project structure
+ - welcome the user
+ - ask user for latitude
+ - ask user for longitude
+ - latitude and longitude into the scraper link
+ - display more details about our location
--- weather_forecast/lib/weather_forecast/api.rb
+++ weather_forecast/lib/weather_forecast/api.rb
@@ -1,9 +1,12 @@
 class WeatherForecast::API
-  attr_accessor
+  attr_accessor :forecast_hash
 
   def self.current_location(latitude,longitude)
-    forecast = RESTCLIENT.get('https://api.darksky.net/forecast/8a97fe8519f98871538db916a4f13fde/37.8267,-122.4233')
-    
+    forecast = RestClient.get('https://api.darksky.net/forecast/8a97fe8519f98871538db916a4f13fde/37.8267,-122.4233')
+    @forecast_hash = JSON.parse(forecast)
+    @forecast_hash["currently"].each do |fore_info|
+      WeatherForecast::Forecast.new_from_json(fore_info)
+    end
   end
 
 end
