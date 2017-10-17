67f90e8dc3e812dc4c0efe9d5a354966537e6472
Breyanna Vass <breyanna.bvass@gmail.com>
2017-10-17 02:32:16 +0000

    Done.

--- weather_forecast/lib/weather_forecast/cli.rb
+++ weather_forecast/lib/weather_forecast/cli.rb
@@ -9,5 +9,6 @@ class WeatherForecast::CLI
     WeatherForecast::API.current_location(latitude,longitude)
   end
 
+#Do NOT forget to edit the WeatherForecast spec file. Record up to 30 minutes of code work for the project. Update blog post and link to repo. Look at the videos for assistance with completing the project and attend the study group for more insight on how to complete the second section of the project to move on to the next section by wednesday.
 
 end
