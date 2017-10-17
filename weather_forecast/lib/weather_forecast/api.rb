645b248cbd5712e14bd1510697c068b94853c686
Breyanna Vass <breyanna.bvass@gmail.com>
2017-10-17 16:38:44 +0000

    Done.

--- weather_forecast/lib/weather_forecast/api.rb
+++ weather_forecast/lib/weather_forecast/api.rb
@@ -2,7 +2,8 @@ class WeatherForecast::API
   attr_accessor
 
   def self.current_location(latitude,longitude)
-
+    forecast = RESTCLIENT.get('https://api.darksky.net/forecast/8a97fe8519f98871538db916a4f13fde/37.8267,-122.4233')
+    
   end
 
 end
