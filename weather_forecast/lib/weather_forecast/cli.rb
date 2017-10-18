40e764b13f807510dba128caa5b73c538c5e8fdf
Breyanna Vass <breyanna.bvass@gmail.com>
2017-10-18 16:57:56 +0000

    Done.

--- weather_forecast/lib/weather_forecast/cli.rb
+++ weather_forecast/lib/weather_forecast/cli.rb
@@ -1,7 +1,7 @@
 class WeatherForecast::CLI
 
   def call
-    puts "Welcome to the weather api"
+    puts "Welcome to the Weather API!"
     puts "Please enter your latitude:"
     latitude = gets.strip.to_f
     puts "Please enter your longitude:"
@@ -9,6 +9,6 @@ class WeatherForecast::CLI
     WeatherForecast::API.current_location(latitude,longitude)
   end
 
-#Do NOT forget to edit the WeatherForecast spec file. Record up to 30 minutes of code work for the project. Update blog post and link to repo. Look at the videos for assistance with completing the project and attend the study group for more insight on how to complete the second section of the project to move on to the next section by wednesday.
+#Record up to 30 minutes of code work for the project. Update blog post. Look at the videos for assistance with completing the project and meeting on wednesday more insight on how to complete the second section of the project.
 
 end
