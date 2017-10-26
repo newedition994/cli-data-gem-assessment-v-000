b0fbf035263300455ef68fb78b61f01980b06e47
Breyanna Vass <breyanna.bvass@gmail.com>
2017-10-26 19:50:26 +0000

    Done.

--- weather_forecast/lib/weather_forecast/cli.rb
+++ weather_forecast/lib/weather_forecast/cli.rb
@@ -8,7 +8,8 @@ class WeatherForecast::CLI
   end
 
   def menu
-    puts "Please choose one of the following cities:"
+    input = nil
+    puts "------ Please choose one of the following cities ------"
     puts "1. New York, New York"
     puts "2. London, England"
     puts "3. Tokyo, Japan"
@@ -19,6 +20,26 @@ class WeatherForecast::CLI
     latitude = WeatherForecast::Forecast.latitude(input)
     longitude = WeatherForecast::Forecast.longitude(input)
     WeatherForecast::API.current_location(latitude, longitude)
+    if input == "exit"
+      goodbye
+    else
+      its = input.to_f
+      if its != 0
+        fore_input = WeatherForecast::Forecast
+        forecast_details(fore_input)
+        puts "Would you like more information for another city?"
+        ci = gets.strip.downcase
+        if ci == "yes" || ci == "y"
+          menu
+        else
+          goodbye
+        end
+      else
+        puts "I don't understand that selection."
+        puts "Please select a city's number, type exit to leave, or get info on your own city"
+        menu
+      end
+    end
     #create a if statement for more details
     #puts "Please enter your latitude:"
     #latitude = gets.strip.to_f
@@ -40,7 +61,7 @@ class WeatherForecast::CLI
   end
 
   def forecast_details(fore_input)
-    puts "___#{fore_input.temperature}___"
+    puts "----#{fore_input.temperature}----"
     puts "Humidity: #{fore_input.humidity}"
     puts "WindSpeed: #{fore_input.windSpeed}"
   end
