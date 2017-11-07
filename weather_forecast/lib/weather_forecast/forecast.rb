89d22de4312447c16fae146427a293557228bc46
Breyanna Vass <breyanna.bvass@gmail.com>
2017-11-07 17:01:14 +0000

    Done.

--- weather_forecast/lib/weather_forecast/forecast.rb
+++ weather_forecast/lib/weather_forecast/forecast.rb
@@ -44,17 +44,19 @@ class WeatherForecast::Forecast
 
   def self.getWeather(input)
     binding.pry
-    case input 
+    case input
     when input == "1"
       WeatherForecast::API.current_location(40.7128, 74.0060, "New York")
     when input == "2"
-      WeatherForecast::API.current_location(51.5074,0.1278, "London, England")
+      puts WeatherForecast::API.current_location(51.5074,0.1278, "London, England")
     when input == "3"
       WeatherForecast::API.current_location(35.6895,139.6917, "Tokyo, Japan")
     when input == "4"
       WeatherForecast::API.current_location(43.6532,79.3832, "Toronto, Canada")
     when input == "5"
       WeatherForecast::API.current_location(22.9068,43.1729, "Rio de Janeiro, Brazil")
+    else
+      puts "Invalid input"
     end
   end
 
