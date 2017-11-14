dbd800f012bf33e783e538323937eb680f76f271
Breyanna Vass <breyanna.bvass@gmail.com>
2017-11-14 21:52:42 +0000

    Done.

---
+++ weather_forecast/lib/environment.rb
@@ -0,0 +1,1 @@
+ENV[key]= "https://api.darksky.net/forecast/18eb3e4c1b0ccabefb97a822895e8aec/#{latitude},#{longitude}" 
--- weather_forecast/lib/weather_forecast.rb
+++ weather_forecast/lib/weather_forecast.rb
@@ -1,9 +1,10 @@
 require 'rest-client'
 require 'json'
 require 'pry'
+require 'dotenv/load'
 
 require_relative "./weather_forecast/version"
 require_relative "./weather_forecast/forecast"
-require_relative "./weather_forecast/environment"
+require_relative "./environment"
 require_relative "./weather_forecast/api"
 require_relative "./weather_forecast/cli"
