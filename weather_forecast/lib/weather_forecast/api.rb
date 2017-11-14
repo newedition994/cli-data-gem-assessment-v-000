125e2ca07027196b58fdada6fae9e6e5a2d03f52
Breyanna Vass <breyanna.bvass@gmail.com>
2017-11-14 21:51:35 +0000

    automatically backed up by learn

--- weather_forecast/lib/weather_forecast/api.rb
+++ weather_forecast/lib/weather_forecast/api.rb
@@ -2,9 +2,10 @@ class WeatherForecast::API
   attr_accessor :forecast_hash, :forecast
 
   def self.current_location(latitude,longitude, location)
-    forecast = RestClient.get("https://api.darksky.net/forecast/8a97fe8519f98871538db916a4f13fde/#{latitude},#{longitude}")
+    forecast = RestClient.get(` echo $key`)
     @forecast_hash = JSON.parse(forecast)
       WeatherForecast::Forecast.new_from_json(@forecast_hash["currently"], location)
   end
 
 end
+#`$key`
